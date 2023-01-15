64023a158b78d302a1342d72e1b442877949c32e
maverickphp <fasih682@gmail.com>
2023-01-15 21:36:56 +0500

    Updated DarkMode

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ import { useState } from 'react';
 
 function App() {
 
-  const [darkmode, setDarkMode] = useState(false); // Wheather enable or not
+  const [mode, setMode] = useState('light'); // Wheather enable or not
+  const toggleMode = ()=>{
+    if (mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor='grey';
+    }
+    else{
+    setMode('light');
+    document.body.style.backgroundColor='white';
+  }
+  }
   return (
 <>
-<Navbar title = "TextAnalyzer" aboutText = "About Mav" Homebutton = "Home" mode={darkmode}/>
+<Navbar title = "TextAnalyzer" aboutText = "About Mav" Homebutton = "Home" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
 <TextForm heading = "Enter Text to analyze"/>
 {/* <About/> */}
