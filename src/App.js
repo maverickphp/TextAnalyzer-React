b6e6105c07ad3fb01c926035f3f592fc6a51acf9
maverickphp <fasih682@gmail.com>
2023-01-15 19:55:11 +0500

    Updated Name

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import { useState } from 'react';
 
 function App() {
+
+  const [darkmode, setDarkMode] = useState(false); // Wheather enable or not
   return (
 <>
-<Navbar title = "MavApp" aboutText = "About Mav" Homebutton = "Home"/>
-
+<Navbar title = "TextAnalyzer" aboutText = "About Mav" Homebutton = "Home" mode={darkmode}/>
 <div className="container my-3">
 <TextForm heading = "Enter Text to analyze"/>
 {/* <About/> */}
